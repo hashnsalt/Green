a161b76d6c467b5f6b78edb230a045ee82e67da3
eun_kyung <dyddydmmlee@gmail.com>
2023-01-13 15:30:17 +0900

    greenWork16

---
+++ SCSS/scss_teacher/scss_teacher/JEST/js/main.test.js
@@ -0,0 +1,22 @@
+const sum = require('./main.js');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
+});
+
+const shoppingList = [
+  'diapers',
+  'kleenex',
+  'trash bags',
+  'paper towels',
+  'milk',
+];
+
+test('the shopping list has milk on it', () => {
+  expect(shoppingList).toContain('milk');
+  expect(new Set(shoppingList)).toContain('milk');
+});
