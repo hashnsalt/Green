6c175b731b12867fca339b7c5d11cf66312f8a30
eun_kyung <dyddydmmlee@gmail.com>
2023-01-16 17:55:23 +0900

    greenWork17

--- GraphQL/index.js
+++ GraphQL/index.js
@@ -13,6 +13,25 @@ const typeDefs = gql`
         equipments: [Equipment]
         supplies: [Supply]
     }
+
+    type Mutation {
+      insertEquipment(
+        id: String,
+        used_by: String,
+        count: Int,
+        new_or_used: String
+      ):Equipment
+      
+      deleteEquipment(id: String): Equipment
+
+      editEquipment(
+        id: String,
+        used_by: String,
+        count: Int,
+        new_or_used: String
+      ):Equipment
+    }
+
     type Team {
         id: Int
         manager: String
@@ -45,6 +64,32 @@ const resolvers = { //gql쿼리에서는 각각의 필드마다 함수가 하나
     .filter((team) => { //조건에 만족하는 데이터를 뽑아내기 위해 사용
       return team.id === args.id //내가 1번이라고 넣는 아이디, 2번이라고 넣는 아이디 database.teams의 값 == args.id와 같다면 출력. 이때 team.id 와 args.id의 값이 같다면 값을 출력하겠다는 말.
     })[0]
+  },
+  Mutation: {//이미 만들어져 있는 쿼리에 편집작업을 하겟다라는 뜻으로 Mutation에 실행문을 넣어줌. 기존의 데이터를 push
+    insertEquipment: (parent, args, context, info) => {
+      database.equipments.push(args) //제일 마지막에 데이터를 push하고 
+      return args //쿼리 결과로 돌림
+    },
+    deleteEquipment: (parent, args, context, info) => {
+      const deleted = database.equipments
+        .filter((equipment) => {
+          return equipment.id === args.id //내가 입력한 객체 데이터와 같은것을 delete에 넣어줌. 이것은 삭제될 데이터
+        })[0]
+        database.equipments = database.equipments
+        .filter((equipment) => {
+            return equipment.id != args.id //삭제한 데이터를 제외하고 남은 데이터를 최종 데이터로 넣어줌.
+          })
+        return deleted
+    },
+    editEquipment: (parent, args, context, info) => {
+      return database.equipments
+        .filter((equipment) => {
+          return equipment.id === args.id
+        }).map((equipment) => {
+          Object.assign(equipment, args)
+          return equipment
+        })[0]
+    }
   }
   //teams를 database.teams로 내보내기 하겠다. 배열 객체로 만든것을 객체로 만들고
 }
